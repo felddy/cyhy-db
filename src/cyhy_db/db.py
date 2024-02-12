474203cc2149d99d4e7453463cf15c9496f570cd
Felddy <markf@geekpad.com>
2024-02-12 13:14:37 -0500

    Add RequestDoc model to ALL_MODELS list

--- src/cyhy_db/db.py
+++ src/cyhy_db/db.py
@@ -1,10 +1,11 @@
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from .models.cve import CVE
+from .models.request_doc import RequestDoc
 from .models.scan_doc import ScanDoc
 from .models.snapshot_doc import SnapshotDoc
 
-ALL_MODELS = [CVE, ScanDoc, SnapshotDoc]
+ALL_MODELS = [CVE, RequestDoc, ScanDoc, SnapshotDoc]
 
 
 async def initialize_db(db_uri: str, db_name: str) -> None:
