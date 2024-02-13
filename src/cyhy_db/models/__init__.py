75124f5ee92b948427f7a0f7d44d99ae0a9614d6
Felddy <markf@geekpad.com>
2024-02-13 12:46:56 -0500

    Sort imports

--- src/cyhy_db/models/__init__.py
+++ src/cyhy_db/models/__init__.py
@@ -1,6 +1,6 @@
 from .cve import CVE
+from .request_doc import RequestDoc
 from .scan_doc import ScanDoc
 from .snapshot_doc import SnapshotDoc
-from .request_doc import RequestDoc
 
 __all__ = ["CVE", "RequestDoc", "ScanDoc", "SnapshotDoc"]
