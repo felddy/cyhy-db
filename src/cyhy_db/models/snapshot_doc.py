7bcf80fa8e07cbe40bd3fb8808e80b77d8ddd4c1
Felddy <markf@geekpad.com>
2024-02-13 12:48:07 -0500

    Add fields and indexes to SnapshotDoc model

--- src/cyhy_db/models/snapshot_doc.py
+++ src/cyhy_db/models/snapshot_doc.py
@@ -1,6 +1,29 @@
 # Third-Party Libraries
 from beanie import Document
+from pydantic import Field
+from pymongo import ASCENDING, IndexModel
+from datetime import datetime
 
 
 class SnapshotDoc(Document):
-    pass
+    owner: str = Field(...)
+    start_time: datetime = Field(...)
+    end_time: datetime = Field(...)
+
+    class Settings:
+        # Beanie settings
+        name = "snapshots"
+        indexes = [
+            IndexModel(
+                [
+                    ("owner", ASCENDING),
+                    ("start_time", ASCENDING),
+                    ("end_time", ASCENDING),
+                ],
+                name="uniques",
+                unique=True,
+            ),
+            IndexModel(
+                [("latest", ASCENDING), ("owner", ASCENDING)], name="latest_owner"
+            ),
+        ]
