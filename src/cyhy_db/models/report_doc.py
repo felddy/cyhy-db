b34588cdb3eb7810799096ecbb055fa046d65fc5
Felddy <markf@geekpad.com>
2024-02-16 14:32:48 -0500

    Add ReportDoc model

---
+++ src/cyhy_db/models/report_doc.py
@@ -0,0 +1,33 @@
+from beanie import Document, Indexed, Link
+from datetime import datetime
+from typing import List
+from pymongo import ASCENDING, IndexModel
+from pydantic import Field
+
+from . import SnapshotDoc
+from .enum import ReportType
+from ..utils import utcnow
+
+
+class ReportDoc(Document):
+    owner: str
+    generated_time: datetime = Field(default_factory=utcnow)
+    snapshots: List[Link[SnapshotDoc]]
+    report_types: List[ReportType]
+
+    class Settings:
+        name = "reports"
+        indexes = [
+            IndexModel(
+                [
+                    ("owner", ASCENDING),
+                ],
+                name="owner",
+            ),
+            IndexModel(
+                [
+                    ("generated_time", ASCENDING),
+                ],
+                name="generated_time",
+            ),
+        ]
