dbf9f1f5d25f9a94ff50d1ed0a800cdb02df4635
Felddy <markf@geekpad.com>
2024-02-16 14:34:23 -0500

    Add VulnScanDoc model

---
+++ src/cyhy_db/models/vuln_scan_doc.py
@@ -0,0 +1,36 @@
+from typing import Dict
+from pymongo import ASCENDING, IndexModel
+from datetime import datetime
+
+from . import ScanDoc
+from .enum import Protocol
+
+
+class VulnScanDoc(ScanDoc):
+    protocol: Protocol
+    port: int
+    service: str
+    cvss_base_score: float
+    cvss_vector: str
+    description: str
+    fname: str
+    plugin_family: str
+    plugin_id: int
+    plugin_modification_date: datetime
+    plugin_name: str
+    plugin_publication_date: datetime
+    plugin_type: str
+    risk_factor: str
+    severity: int
+    solution: str
+    synopsis: str
+
+    class Settings:
+        # Beanie settings
+        name = "vuln_scans"
+        indexes = [
+            IndexModel(
+                [("owner", ASCENDING), ("latest", ASCENDING), ("severity", ASCENDING)],
+                name="owner_latest_severity",
+            ),
+        ]
