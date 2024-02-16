2ba2f5be16ace5d92c65f2060584bd1396a54097
Felddy <markf@geekpad.com>
2024-02-16 14:31:41 -0500

    Refactor and partially implement helpers with deprecation

--- src/cyhy_db/models/host_doc.py
+++ src/cyhy_db/models/host_doc.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, Optional, Tuple
 import random
 from .enum import Stage, Status
 from ipaddress import IPv4Address, ip_address
+from ..utils import deprecated, utcnow
 
 
 class State(BaseModel):
@@ -17,7 +18,7 @@ class HostDoc(Document):
     id: int = Field(...)  # IP address as an integer
     ip: IPv4Address = Field(...)
     owner: str = Field(...)
-    last_change: datetime = Field(default_factory=datetime.utcnow)
+    last_change: datetime = Field(default_factory=utcnow)
     next_scan: Optional[datetime] = Field(default=None)
     state: State = Field(default_factory=lambda: State(reason="new", up=False))
     stage: Stage = Field(default=Stage.NETSCAN1)
@@ -36,9 +37,10 @@ class HostDoc(Document):
 
     @before_event(Insert, Replace, ValidateOnSave)
     async def before_save(self):
-        self.last_change = datetime.utcnow()
+        self.last_change = utcnow()
 
     class Settings:
+        # Beanie settings
         name = "hosts"
         indexes = [
             IndexModel(
@@ -88,3 +90,59 @@ class HostDoc(Document):
                 name="latest_scan_done",
             ),
         ]
+
+    def set_state(self, nmap_says_up, has_open_ports, reason=None):
+        """Sets state.up based on different stage
+        evidence. nmap has a concept of up which is
+        different from our definition. An nmap "up" just
+        means it got a reply, not that there are any open
+        ports. Note either argument can be None."""
+
+        if has_open_ports == True:  # Only PORTSCAN sends in has_open_ports
+            self.state = State(True, "open-port")
+        elif has_open_ports == False:
+            self.state = State(False, "no-open")
+        elif nmap_says_up == False:  # NETSCAN says host is down
+            self.state = State(False, reason)
+
+    # TODO: There are a lot of functions in the Python 2 version that may or may not be used.
+    #       Instead of porting them all over, we should just port them as they are needed.
+    #       And rewrite things that can be done better in Python 3.
+
+    # @classmethod
+    # async def get_count(cls, owner: str, stage: Stage, status: Status):
+    #     return await cls.count(
+    #         cls.owner == owner, cls.stage == stage, cls.status == status
+    #     )
+
+    @classmethod
+    @deprecated("Use HostDoc.find_one(HostDoc.ip == ip) instead.")
+    async def get_by_ip(cls, ip: IPv4Address):
+        return await cls.find_one(cls.ip == ip)
+
+    # @classmethod
+    # @deprecated("Use HostDoc.find_one(HostDoc.ip == ip).owner instead.")
+    # async def get_owner_of_ip(cls, ip: IPv4Address):
+    #     host = await cls.get_by_ip(ip)
+    #     return host.owner
+
+    # @classmethod
+    # async def get_some_for_stage(
+    #     cls,
+    #     stage: Stage,
+    #     count: int,
+    #     owner: Optional[str] = None,
+    #     waiting: bool = False,
+    # ):
+    #     if waiting:
+    #         status = {"$in": [Status.READY, Status.WAITING]}
+    #     else:
+    #         status = Status.READY
+
+    #     query = cls.find(cls.status == status, cls.stage == stage)
+    #     if owner is not None:
+    #         query = query.find(cls.owner == owner)
+
+    #     # Sorting and limiting the results
+    #     results = await query.sort([("priority", 1), ("r", 1)]).limit(count).to_list()
+    #     return results
