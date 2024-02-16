c47f61f82b47e9ef85e92f078af0fdfaabdc8a26
Felddy <markf@geekpad.com>
2024-02-16 14:32:36 -0500

    Add PortScanDoc model with indexes

---
+++ src/cyhy_db/models/port_scan_doc.py
@@ -0,0 +1,35 @@
+from typing import Dict
+from pymongo import ASCENDING, IndexModel
+
+from . import ScanDoc
+from .enum import Protocol
+
+
+class PortScanDoc(ScanDoc):
+    protocol: Protocol
+    port: int
+    service: Dict = {}  # Assuming no specific structure for "service"
+    state: str
+    reason: str
+
+    class Settings:
+        # Beanie settings
+        name = "port_scans"
+        indexes = [
+            IndexModel(
+                [("latest", ASCENDING), ("owner", ASCENDING), ("state", ASCENDING)],
+                name="latest_owner_state",
+            ),
+            IndexModel(
+                [("latest", ASCENDING), ("service.name", ASCENDING)],
+                name="latest_service_name",
+            ),
+            IndexModel(
+                [("latest", ASCENDING), ("time", ASCENDING)],
+                name="latest_time",
+            ),
+            IndexModel(
+                [("owner", ASCENDING)],
+                name="owner",
+            ),
+        ]
