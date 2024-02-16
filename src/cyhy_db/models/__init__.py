1fab95bc73a24bc07416edd4144e01d447939e3b
Felddy <markf@geekpad.com>
2024-02-16 14:29:53 -0500

    Reorder model imports to avoid circular references

--- src/cyhy_db/models/__init__.py
+++ src/cyhy_db/models/__init__.py
@@ -1,7 +1,35 @@
+# Scan documents (order matters)
+from .scan_doc import ScanDoc
+from .host_scan_doc import HostScanDoc
+from .port_scan_doc import PortScanDoc
+from .vuln_scan_doc import VulnScanDoc
+
+# Snapshot documents (order matters)
+from .snapshot_doc import SnapshotDoc
+from .report_doc import ReportDoc
+
+# Other documents
 from .cve import CVE
 from .host_doc import HostDoc
+from .kev_doc import KEVDoc
+from .place_doc import PlaceDoc
 from .request_doc import RequestDoc
-from .scan_doc import ScanDoc
-from .snapshot_doc import SnapshotDoc
+from .system_control_doc import SystemControlDoc
+from .tally_doc import TallyDoc
 
-__all__ = ["CVE", "HostDoc", "RequestDoc", "ScanDoc", "SnapshotDoc"]
+
+__all__ = [
+    "CVE",
+    "HostDoc",
+    "HostScanDoc",
+    "KEVDoc",
+    "PlaceDoc",
+    "PortScanDoc",
+    "RequestDoc",
+    "ReportDoc",
+    "ScanDoc",
+    "SnapshotDoc",
+    "SystemControlDoc",
+    "TallyDoc",
+    "VulnScanDoc",
+]
